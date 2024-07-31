10223a48d937b39db1b30bacdf40b89a05590810
breeze807601 <2072063341@qq.com>
2024-07-31 15:20:37 +0800

    main.js配置更改

--- src/main.js
+++ src/main.js
@@ -6,15 +6,11 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
-import UndrawUi from 'undraw-ui'
-import 'undraw-ui/dist/style.css'
-
 const app = createApp(App)
 
 app.use(store)
 app.use(router)
 app.use(ElementPlus)
-app.use(UndrawUi)
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
