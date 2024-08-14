558586d9cb442faf8bfa95f9e16abaeca94062ad
breeze807601 <2072063341@qq.com>
2024-08-14 15:00:18 +0800

    个人主页标签栏的对应信息变化

--- src/components/userRelational/Webpage.vue
+++ src/components/userRelational/Webpage.vue
@@ -7,15 +7,15 @@
                 </div>
                 <el-menu-item index="/myPage">
                     <el-icon><user /></el-icon>
-                    <template #title>我的主页</template>
+                    <template #title>{{ isSelf ? "我的主页" : "TA的主页" }}</template>
                 </el-menu-item>
                 <el-menu-item index="/myAttention">
                     <el-icon><Star /></el-icon>
-                    <template #title>我的关注</template>
+                    <template #title>{{ isSelf ? "我的关注" : "TA的关注" }}</template>
                 </el-menu-item>
                 <el-menu-item index="/myFans">
                     <el-icon><Fries /></el-icon>
-                    <template #title>我的粉丝</template>
+                    <template #title>{{ isSelf ? "我的粉丝" : "TA的粉丝" }}</template>
                 </el-menu-item>
             </el-menu>
         </el-col>
@@ -28,6 +28,19 @@
 <script setup>
 document.title = "社交媒体平台-个人主页";
 import router from "@/router";
+import {onMounted, ref} from "vue";
+
+onMounted( () => {
+    isMe()
+})
+const isSelf = ref(false)   // true: 查看的是本人的主页，false: 查看的是其他人的主页
+let userId = ref(localStorage.getItem('id'))   // 需要查看的id
+
+async function isMe() {
+    if (JSON.parse(localStorage.getItem("userInfo")).id === userId.value) {
+        isSelf.value = true
+    }
+}
 
 function handleSelect(key) {
     // 根据 key 的值来决定跳转到哪个路由
